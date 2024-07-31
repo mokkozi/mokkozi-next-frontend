41762d71da56f39842d5238704b18de8f4eefc12
c9u11 <tjrdud6412@naver.com>
2024-07-31 17:47:12 +0900

    💄 Title, SubTitle, Bold

---
+++ app/components/Typo.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import styled from "styled-components";
+
+export const Title = styled.h1`
+  font-size: 20px;
+  margin: 0;
+  color: #000;
+  font-weight: 700;
+`;
+
+export const SubTitle = styled.h2`
+  font-size: 12px;
+  margin: 0;
+  color: #6c7072;
+`;
+
+export const Bold = styled.span`
+  font-weight: 700;
+`;
