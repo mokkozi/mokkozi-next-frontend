cdeab2fe28408a187b98b1fbc2d6d7313ee6ddc0
c9u11 <tjrdud6412@naver.com>
2024-08-01 09:45:23 +0900

    🐛 Jest + SVG 문제 해결

--- jest.config.ts
+++ jest.config.ts
@@ -3,16 +3,28 @@ import nextJest from "next/jest.js";
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-  dir: "./",
+  dir: "./"
 });
 
 // Add any custom config to be passed to Jest
 const config: Config = {
   coverageProvider: "v8",
-  testEnvironment: "jsdom",
+  testEnvironment: "jsdom"
   // Add more setup options before each test is run
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 };
+const jestConfigWithOverrides = async () => {
+  const configFn = createJestConfig(config);
+  const res = await configFn();
 
+  res.moduleNameMapper = {
+    // We cannot depend on the exact key used by Next.js
+    // so we inject an SVG key higher up on the mapping tree
+    "\\.svg": "<rootDir>/__mocks__/svg.js",
+    ...res.moduleNameMapper
+  };
+
+  return res;
+};
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-export default createJestConfig(config);
+export default jestConfigWithOverrides;
