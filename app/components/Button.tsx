7b8adca4c9fc2122e01c03ea5d9723727aa53801
c9u11 <tjrdud6412@naver.com>
2024-08-17 23:45:45 +0900

    💄 Toggle Button Component
    - Input Checkbox를 사용하는 Toggle Button Component 개발
    - checked 사용 유무와 관계없이 동작하도록 설계

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,14 +1,50 @@
 "use client";
 
+import { InputHTMLAttributes } from "react";
 import styled from "styled-components";
 
 export const PrimaryButton = styled.button`
   font-size: ${(props) => props.theme.fonts.size.medium};
   background-color: ${(props) => props.theme.colors.highlight};
   color: ${(props) => props.theme.colors.white};
-  padding: 10px 0px;
+  padding: 10px;
   border-radius: 5px;
   border: none;
   cursor: pointer;
   width: 100%;
 `;
+
+const ToggleButtonContainer = styled.label`
+  position: relative;
+  display: inline-block;
+`;
+
+const ToggleButtonInput = styled.input`
+  opacity: 0;
+  width: 0;
+  height: 0;
+`;
+
+const ToggleButtonStyle = styled.div`
+  font-size: ${(props) => props.theme.fonts.size.medium};
+  background-color: ${(props) => props.theme.colors.white};
+  padding: 10px;
+  border-radius: 5px;
+  color: ${(props) => props.theme.colors.gray};
+  border: 1px solid ${(props) => props.theme.colors.darkWhite};
+  cursor: pointer;
+  input:checked + & {
+    color: ${(props) => props.theme.colors.highlight};
+    border: 1px solid ${(props) => props.theme.colors.highlight};
+  }
+`;
+
+export const ToggleButton = (props: InputHTMLAttributes<HTMLInputElement>) => {
+  const { style, children, ...rest } = props;
+  return (
+    <ToggleButtonContainer>
+      <ToggleButtonInput type="checkbox" {...rest} />
+      <ToggleButtonStyle style={style}>{children}</ToggleButtonStyle>
+    </ToggleButtonContainer>
+  )
+}
