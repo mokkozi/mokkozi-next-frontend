ec3b16ea7cadb219ede6f70e0a68b56721981d4f
c9u11 <tjrdud6412@naver.com>
2024-08-25 20:56:54 +0900

    💄 BottomBar

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -8,3 +8,13 @@ export const TopBar = styled.div`
   padding: 14px 18px;
   border-bottom: 1px solid ${(props) => props.theme.colors.lightGray};
 `;
+
+export const BottomBar = styled.div`
+  width: 100%;
+  height: 56px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  padding: 14px 18px;
+  box-shadow: 0px -1px 1px rgba(20,20,20,0.02), 0px -2px 8px rgba(20,20,20,0.04);
+`;
